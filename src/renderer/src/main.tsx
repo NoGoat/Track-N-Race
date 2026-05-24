a94b757702efb27e83e5ee151fdd14384ed8ef23
NoGoat <eagleeyes007123@gmail.com>
2026-05-25 01:09:22 +0530

    Added React Scan for dev purposes

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,8 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { scan } from 'react-scan'
 import 'uplot/dist/uPlot.min.css'
 import './index.css'
 import App from './App.tsx'
+
+if (import.meta.env.DEV) {
+  scan({ enabled: true })
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
